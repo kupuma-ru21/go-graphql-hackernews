0fe97617995946ceed0c2f389ac1bfc6a419bd52
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-11 01:39:09 +0900

    fix: resolver of CreateLink

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -8,17 +8,17 @@ import (
 	"context"
 	"fmt"
 	"hackernews/graph/model"
+	"hackernews/internal/links"
+	"strconv"
 )
 
 // CreateLink is the resolver for the createLink field.
 func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
-	var link model.Link
-	var user model.User
-	link.Address = input.Address
+	var link links.Link
 	link.Title = input.Title
-	user.Name = "test"
-	link.User = &user
-	return &link, nil
+	link.Address = input.Address
+	linkID := link.Save()
+	return &model.Link{ID: strconv.FormatInt(linkID, 10), Title: link.Title, Address: link.Address}, nil
 }
 
 // CreateUser is the resolver for the createUser field.
